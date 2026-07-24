753174c98055e49ef878e5ec4e8a787660d67e54
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2026-07-24 00:48:31 -0300

    2703: return length arguments passed

---
+++ 2703-return-length-args-passed.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ * Instructions:
+ * 
+ * Write a function argumentsLength that returns the count of arguments passed to it.
+ * 
+ * ---
+ * 
+ * Time complexity: O(1)
+ * 
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function (...args) {
+  return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
