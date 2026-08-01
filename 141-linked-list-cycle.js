c38ff68e1ed3f9642b54ac005cd3e12e0b11b86a
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2026-08-01 00:04:20 -0300

    141: linked list cycle

---
+++ 141-linked-list-cycle.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ * Instructions:
+ * 
+ * Given head, the head of a linked list, determine if the linked list has a cycle in it.
+ * 
+ * There is a cycle in a linked list if there is some node in the list that can be reached
+ * again by continuously following the next pointer. Internally, pos is used to denote the
+ * index of the node that tail's next pointer is connected to. Note that pos is not passed
+ * as a parameter.
+ * 
+ * Return true if there is a cycle in the linked list. Otherwise, return false.
+ * 
+ * ---
+ * 
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    const existing = new Set()
+    let pivot = head;
+
+    while (pivot && pivot.next) {
+        if (existing.has(pivot.next)) {
+            return true
+        }
+        existing.add(pivot.next)
+        pivot = pivot.next
+    }
+    return false
+};ƒ
